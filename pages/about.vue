46be83e073e50f8a30bbb5869794f7b91973453d
ikemura <k_ikemura@yumemi.co.jp>
2017-12-22 15:09:00 +0900

    vuexの値をボタンで操作する

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,13 @@
 <template>
   <section class="container">
     <div>
+      <p>
+        <button @click="$store.commit('increment')">{{ $store.state.counter }}</button><br>
+      </p>
       <h1 class="title">
         {{ $store.state.counter }}
       </h1>
     </div>
+    <nuxt-link to="/">Home</nuxt-link>
   </section>
 </template>
